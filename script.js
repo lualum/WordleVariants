541f98bfcc63a1ed23597f618c4db6deb63bd033
lualum <lucas.a.lum@gmail.com>
2026-01-21 09:02:59 -0800

    prevents flashing during rapid reload

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 import { TARDLEGame } from "./game.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
-   const gameContainer = document.getElementById("game-container");
+   const gameAppContainer = document.getElementById("game-app-container");
 
    const game = new TARDLEGame();
    window.game = game;
 
-   gameContainer.classList.add("loaded");
+   gameAppContainer.classList.add("loaded");
 });
